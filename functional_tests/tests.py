7c6526f03060e2b07280e25af2c89ea7c539fdda
Siri Kijjaboriboon <s6101012620164@email.kmutnb.ac.th>
2020-01-30 05:18:56 +0700

    Write some user story in functional_tests and index.html

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):  
+
+    def setUp(self):  
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_using_chat_app(self):  
+        # Antony want to chat with his friend and he heard their have a new web chat. 
+        # He go to check out this web .
+        self.browser.get('http://127.0.0.1:8000/chat')
+
+        # He notices the page title and header mention to-do lists
+        self.assertIn('Edu-Match', self.browser.title) 
+
+        self.fail('Finish the test!')
